f845080ab482ec47dad3df0e5898db655c91a635
Eren Gölge <egolge@coqui.ai>
2021-11-04 13:17:59 +0100

    Implement MLFlow logger

---
+++ trainer/logging/base_dash_logger.py
@@ -0,0 +1,74 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Union
+
+
+class BaseDashboardLogger(ABC):
+
+    @abstractmethod
+    def add_scalar(self, title:str, value:float, step:int) -> None:
+        pass
+
+    @abstractmethod
+    def add_figure(self, title:str, figure:Union["matplotlib.figure.Figure", "plotly.graph_objects.Figure"], step:int) -> None:
+        pass
+
+    @abstractmethod
+    def add_audio(self, title:str, audio:"np.ndarray", step:int, sample_rate:int) -> None:
+        pass
+
+    @abstractmethod
+    def add_text(self, title:str, text:str, step:int) -> None:
+        pass
+
+    @abstractmethod
+    def add_artifact(self, file_or_dir:str, name:str, artifact_type:str, aliases=None):
+        pass
+
+    @abstractmethod
+    def add_scalars(self, scope_name:str, scalars:Dict, step:int):
+        pass
+
+    @abstractmethod
+    def add_figures(self, scope_name:str, figures:Dict, step:int):
+        pass
+
+    @abstractmethod
+    def add_audios(self, scope_name:str, audios:Dict, step:int):
+        pass
+
+    @abstractmethod
+    def flush():
+        pass
+
+    @abstractmethod
+    def finish():
+        pass
+
+    def train_step_stats(self, step, stats):
+        self.add_scalars(f"{self.model_name}_TrainIterStats", stats, step)
+
+    def train_epoch_stats(self, step, stats):
+        self.add_scalars(f"{self.model_name}_TrainEpochStats", stats, step)
+
+    def train_figures(self, step, figures):
+        self.add_figures(f"{self.model_name}_TrainFigures", figures, step)
+
+    def train_audios(self, step, audios, sample_rate):
+        self.add_audios(f"{self.model_name}_TrainAudios", audios, step, sample_rate)
+
+    def eval_stats(self, step, stats):
+        self.add_scalars(f"{self.model_name}_EvalStats", stats, step)
+
+    def eval_figures(self, step, figures):
+        self.add_figures(f"{self.model_name}_EvalFigures", figures, step)
+
+    def eval_audios(self, step, audios, sample_rate):
+        self.add_audios(f"{self.model_name}_EvalAudios", audios, step, sample_rate)
+
+    def test_audios(self, step, audios, sample_rate):
+        self.add_audios(f"{self.model_name}_TestAudios", audios, step, sample_rate)
+
+    def test_figures(self, step, figures):
+        self.add_figures(f"{self.model_name}_TestFigures", figures, step)
+
+
---
+++ trainer/logging/mlflow_logger.py
@@ -0,0 +1,131 @@
+import os
+import traceback
+
+from mlflow.tracking import MlflowClient
+from mlflow.utils.mlflow_tags import MLFLOW_RUN_NAME
+from mlflow.tracking.context.registry import resolve_tags
+from trainer.logging.base_dash_logger import BaseDashboardLogger
+from trainer.utils.distributed import rank_zero_only
+import soundfile as sf
+import tempfile
+import shutil
+import torch
+
+
+class MLFlowLogger(BaseDashboardLogger):
+    def __init__(self, log_uri:str, model_name:str, tags:str=None, ):
+        self.model_name = model_name
+        self.client = MlflowClient(tracking_uri=os.path.join(log_uri))
+
+        experiment = self.client.get_experiment_by_name(model_name)
+        if experiment is None:
+            self.experiment_id = self.client.create_experiment(name=model_name)
+        else:
+            self.experiment_id = experiment.experiment_id
+
+        if tags is not None:
+            self.client.set_experiment_tag(self.experiment_id, MLFLOW_RUN_NAME, tags)
+        run = self.client.create_run(experiment_id=self.experiment_id, tags=resolve_tags(tags))
+        self.run_id = run.info.run_id
+
+    def model_weights(self, model, step):
+        layer_num = 1
+        for name, param in model.named_parameters():
+            if param.numel() == 1:
+                self.client.log_metric("layer{}-{}/value".format(layer_num, name), param.max(), step)
+            else:
+                self.client.log_metric("layer{}-{}/max".format(layer_num, name), param.max(), step)
+                self.client.log_metric("layer{}-{}/min".format(layer_num, name), param.min(), step)
+                self.client.log_metric("layer{}-{}/mean".format(layer_num, name), param.mean(), step)
+                self.client.log_metric("layer{}-{}/std".format(layer_num, name), param.std(), step)
+                # MlFlow does not support histograms
+                # self.client.add_histogram("layer{}-{}/param".format(layer_num, name), param, step)
+                # self.client.add_histogram("layer{}-{}/grad".format(layer_num, name), param.grad, step)
+            layer_num += 1
+
+    def add_scalar(self, title, value, step):
+        self.client.log_metric(self.run_id, title, value, step)
+
+    def add_text(self, title, text, step):
+        self.client.log_text(self.run_id,text, "{}/{}.txt".format(title, step))
+
+    def add_figure(self, title, figure, step):
+        self.client.log_figure(figure, "{}/{}.png".format(title, step))
+
+    def add_artifact(self, file_or_dir, name, artifact_type, aliases=None):  # pylint: disable=W0613, R0201
+        self.client.log_artifacts(self.run_id, file_or_dir)
+
+    def add_audio(self, title, audio, step, sample_rate):
+        self.client.log_audio(self.run_id, audio, "{}/{}.wav".format(title, step), sample_rate)
+
+    @rank_zero_only
+    def add_scalars(self, scope_name, stats, step):
+        for key, value in stats.items():
+            if torch.is_tensor(value):
+                value = value.item()
+            self.client.log_metric(self.run_id, "{}-{}".format(scope_name, key), value, step)
+
+    @rank_zero_only
+    def add_figures(self, scope_name, figures, step):
+        for key, value in figures.items():
+            self.client.log_figure(self.run_id, value, "{}/{}/{}.png".format(scope_name, key, step))
+
+    @rank_zero_only
+    def add_audios(self, scope_name, audios, step, sample_rate):
+        for key, value in audios.items():
+            if value.dtype == "float16":
+                value = value.astype("float32")
+            try:
+                tmp_audio_path = tempfile.NamedTemporaryFile(suffix=".wav")
+                sf.write(tmp_audio_path, value, sample_rate)
+                self.client.log_artifact(self.run_id, tmp_audio_path, "{}/{}/{}.wav".format(scope_name, key, step))
+                shutil.rmtree(tmp_audio_path)
+            except RuntimeError:
+                traceback.print_exc()
+
+    def train_step_stats(self, step, stats):
+        self.client.set_tag(self.run_id, "Mode", "training")
+        super().train_step_stats(step, stats)
+
+    def train_epoch_stats(self, step, stats):
+        self.client.set_tag(self.run_id, "Mode", "training")
+        super().train_epoch_stats(step, stats)
+
+    def train_figures(self, step, figures):
+        self.client.set_tag(self.run_id, "Mode", "training")
+        super().train_figures(step, figures)
+
+    def train_audios(self, step, audios, sample_rate):
+        self.client.set_tag(self.run_id, "Mode", "training")
+        super().train_audios(step, audios, sample_rate)
+
+    def eval_stats(self, step, stats):
+        self.client.set_tag(self.run_id, "Mode", "evaluation")
+        super().eval_stats(step, stats)
+
+    def eval_figures(self, step, figures):
+        self.client.set_tag(self.run_id, "Mode", "evaluation")
+        super().eval_figures(step, figures)
+
+    def eval_audios(self, step, audios, sample_rate):
+        self.client.set_tag(self.run_id, "Mode", "evaluation")
+        super().eval_audios(step, audios, sample_rate)
+
+    def test_audios(self, step, audios, sample_rate):
+        self.client.set_tag(self.run_id, "Mode", "test")
+        super().test_audios(step, audios, sample_rate)
+
+    def test_figures(self, step, figures):
+        self.client.set_tag(self.run_id, "Mode", "test")
+        super().test_figures(step, figures)
+
+
+    def flush(self):
+        pass
+
+    @rank_zero_only
+    def finish(self):
+        super().finalize(status)
+        status = "FINISHED" if status == "success" else status
+        if self.client.get_run(self.run_id):
+            self.client.set_terminated(self.run_id, status)
