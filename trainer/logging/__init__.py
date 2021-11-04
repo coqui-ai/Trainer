324c81426cf96ddf91577413ecc76c8769a01138
Eren Gölge <egolge@coqui.ai>
2021-11-04 13:17:13 +0100

    Init MLFlow logger

--- trainer/logging/__init__.py
+++ trainer/logging/__init__.py
@@ -1,4 +1,6 @@
+import os
 from trainer.logging.console_logger import ConsoleLogger
+from trainer.logging.mlflow_logger import MLFlowLogger
 from trainer.logging.tensorboard_logger import TensorboardLogger
 from trainer.logging.wandb_logger import WandbLogger
 
@@ -10,7 +12,7 @@ def init_dashboard_logger(config):
         dashboard_logger = TensorboardLogger(config.output_log_path, model_name=project_name)
 
     elif config.dashboard_logger == "wandb":
-        if config.project_name:
+        if "project_name" in config:
             project_name = config.project_name
 
         dashboard_logger = WandbLogger(
@@ -19,6 +21,13 @@ def init_dashboard_logger(config):
             config=config,
             entity=config.wandb_entity,
         )
+
+    elif config.dashboard_logger == "mlflow":
+        if "project_name" in config:
+            project_name = config.project_name
+
+        log_uri = config.get("mlflow_uri", os.environ["MLFLOW_TRACKING_URI"])
+        dashboard_logger = MLFlowLogger(log_uri=log_uri, model_name=project_name)
 
     dashboard_logger.add_text("model-config", f"<pre>{config.to_json()}</pre>", 0)
     return dashboard_logger
