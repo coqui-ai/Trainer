764228109c90971c82595fcaa951ef11257771dd
Eren Gölge <egolge@coqui.ai>
2022-03-11 11:38:55 +0100

    Add templates

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,1 +1,8 @@
 from trainer.trainer import *
+
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "VERSION"), "r", encoding="utf-8") as f:
+    version = f.read().strip()
+
+__version__ = version
