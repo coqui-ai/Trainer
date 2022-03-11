0b84b6e960f23d0369fe4c52459ecc5789b2f7fa
Eren Gölge <egolge@coqui.ai>
2022-03-11 11:41:31 +0100

    Issue_templates (#9)
    * Add templates
    
    * Add config

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,2 +1,9 @@
 from trainer.trainer import *
 from trainer.model import *
+
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "VERSION"), "r", encoding="utf-8") as f:
+    version = f.read().strip()
+
+__version__ = version
