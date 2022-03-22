de673ca9149c6dcdda1342558a7c011844476889
WeberJulian <julian.weber@hotmail.fr>
2022-03-22 12:00:44 +0100

    Add continue train tests

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+def run_cli(command):
+    exit_status = os.system(command)
+    assert exit_status == 0, f" [!] command `{command}` failed."
---
+++ tests/test_continue_train.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import shutil
+from trainer import get_last_checkpoint
+from tests import run_cli
+
+def test_continue_train():
+    output_path = "output/"
+
+    command_train = f"python tests/utils/train_mnist.py"
+    run_cli(command_train)
+
+    continue_path = max(glob.glob(os.path.join(output_path, "*/")), key=os.path.getmtime)
+    number_of_checkpoints = len(glob.glob(os.path.join(continue_path, "*.pth")))
+
+    command_continue = f"python tests/utils/train_mnist.py --continue_path {continue_path}"
+    run_cli(command_continue)
+
+    assert number_of_checkpoints < len(glob.glob(os.path.join(continue_path, "*.pth")))
+    shutil.rmtree(continue_path)
