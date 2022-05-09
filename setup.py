3711862afef4436ead65576b08456fdc33f53996
Eren Gölge <egolge@coqui.ai>
2022-05-09 09:57:14 +0200

    Update for p3.10 (#38)
    * Update python req to p3.10
    
    * Set cudnn_benchmark default False
    
    It causes faster iterations with changing
    sequence lengths in a dataset
    
    * Add CI for p3.10

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ from setuptools import find_packages, setup
 
 if LooseVersion(sys.version) < LooseVersion("3.6") or LooseVersion(
     sys.version
-) > LooseVersion("3.10"):
+) > LooseVersion("3.11"):
     raise RuntimeError(
-        "Trainer requires python >= 3.6 and <=3.10 "
+        "Trainer requires python >= 3.6 and <=3.11 "
         "but your Python version is {}".format(sys.version)
     )
 
@@ -105,7 +105,7 @@ setup(
         "test": requirements_test,
         "all": requirements_all
     },
-    python_requires=">=3.6.0, <3.10",
+    python_requires=">=3.6.0, <3.11",
     classifiers=[
         "Environment :: Console",
         "Natural Language :: English",
@@ -123,6 +123,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     zip_safe=False,
 )
