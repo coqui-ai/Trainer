d465b00d2b883c85a022334d555b195bd9650375
Eren G??lge <egolge@coqui.ai>
2022-06-03 12:47:13 +0200

    Fix test_num_gpu

--- tests/test_num_gpus.py
+++ tests/test_num_gpus.py
@@ -1,43 +1,46 @@
 import os
+
+import unittest
 from unittest import TestCase, mock
 
 from trainer import TrainerArgs
 from trainer.distribute import get_gpus
 
+from argparse import Namespace
 
 class TestGpusStringParsingMethods(TestCase):
     @mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0"})
     def test_parse_gpus_set_in_env_var_and_args(self):
-        parsed_args = create_args_parser().parse_args(["--gpus", "0,1"])
-        gpus = get_gpus(parsed_args)
+        args = Namespace(gpus="0,1")
+        gpus = get_gpus(args)
         expected_value = ["0"]
         self.assertEqual(expected_value, gpus, msg_for_test_failure(expected_value))
 
     @mock.patch.dict(os.environ, {})
     def test_parse_gpus_set_in_args(self):
-        parsed_args = create_args_parser().parse_args(["--gpus", "0,1"])
-        gpus = get_gpus(parsed_args)
+        args = Namespace(gpus="0,1")
+        gpus = get_gpus(args)
         expected_value = ["0", "1"]
         self.assertEqual(expected_value, gpus, msg_for_test_failure(expected_value))
 
     @mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1"})
     def test_parse_gpus_set_in_env_var(self):
-        parsed_args = create_args_parser().parse_args(None)
-        gpus = get_gpus(parsed_args)
+        args = Namespace()
+        gpus = get_gpus(args)
         expected_value = ["0", "1"]
         self.assertEqual(expected_value, gpus, msg_for_test_failure(expected_value))
 
     @mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0, 1 "})
     def test_parse_gpus_set_in_env_var_with_spaces(self):
-        parsed_args = create_args_parser().parse_args(None)
-        gpus = get_gpus(parsed_args)
+        args = Namespace()
+        gpus = get_gpus(args)
         expected_value = ["0", "1"]
         self.assertEqual(expected_value, gpus, msg_for_test_failure(expected_value))
 
     @mock.patch.dict(os.environ, {})
     def test_parse_gpus_set_in_args_with_spaces(self):
-        parsed_args = create_args_parser().parse_args(["--gpus", "0, 1, 2, 3 "])
-        gpus = get_gpus(parsed_args)
+        args = Namespace(gpus="0, 1, 2, 3 ")
+        gpus = get_gpus(args)
         expected_value = ["0", "1", "2", "3"]
         self.assertEqual(expected_value, gpus, msg_for_test_failure(expected_value))
 
@@ -50,3 +53,6 @@ def create_args_parser():
     parser = TrainerArgs().init_argparse(arg_prefix="")
     parser.add_argument("--gpus", type=str)
     return parser
+
+if __name__ == "__main__":
+    unittest.main()
