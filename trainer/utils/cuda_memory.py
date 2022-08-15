64fb48aaa1314c1310e2488bbd201af2ecdf9235
loganhart420 <loganhartpersonal@gmail.com>
2022-08-15 05:41:24 -0400

    Add missing function

--- trainer/utils/cuda_memory.py
+++ trainer/utils/cuda_memory.py
@@ -43,7 +43,7 @@ def get_cuda_blocked_memory():
     current_block = available_memory - 2 ** 28  # 256 MB steps
     while True:
         try:
-            block = torch.empty((current_block,), dtype=torch.uint8, device="cuda")
+            _ = torch.empty((current_block,), dtype=torch.uint8, device="cuda")
             break
         except RuntimeError as exception:
             if is_cuda_out_of_memory(exception):
@@ -52,23 +52,24 @@ def get_cuda_blocked_memory():
                     return available_memory
             else:
                 raise
-    block = None
+    _ = None
     gc_cuda()
     return available_memory - current_block
 
 
 def is_cuda_out_of_memory(exception):
     return (
-        isinstance(exception, RuntimeError) and len(exception.args) == 1 and "CUDA out of memory." in exception.args[0]
+            isinstance(exception, RuntimeError) and len(exception.args) == 1 and "CUDA out of memory." in
+            exception.args[0]
     )
 
 
 def is_cudnn_snafu(exception):
     # For/because of https://github.com/pytorch/pytorch/issues/4107
     return (
-        isinstance(exception, RuntimeError)
-        and len(exception.args) == 1
-        and "cuDNN error: CUDNN_STATUS_NOT_SUPPORTED." in exception.args[0]
+            isinstance(exception, RuntimeError)
+            and len(exception.args) == 1
+            and "cuDNN error: CUDNN_STATUS_NOT_SUPPORTED." in exception.args[0]
     )
 
 
@@ -86,3 +87,7 @@ def cuda_meminfo():
         torch.cuda.max_memory_reserved() / 2 ** 30,
         "GB",
     )
+
+
+def should_reduce_batch_size(exception):
+    return is_cuda_out_of_memory(exception) or is_cudnn_snafu(exception)
